f38c127af53ff85ef3f5a95c52b4c4a8477f76df
denisablajan <deniblajan93@gmail.com>
2021-04-08 19:42:15 +0300

    Lab7

--- Lab5/lab02_clocking_blocks/instr_register_test.sv
+++ Lab5/lab02_clocking_blocks/instr_register_test.sv
@@ -20,19 +20,33 @@ module instr_register_test (tb_ifc io);  // interface port
   int seed = 555;
 
   class Transaction;
-  opcode_t       opcode;
-  operand_t      operand_a, operand_b;
+  rand opcode_t     opcode;
+  rand operand_t     operand_a, operand_b;
   address_t      write_pointer;
 
-  function void randomize_transaction();
-    static int temp = 0;
-    operand_a     = $random(seed)%16;                 // between -15 and 15
-    operand_b     = $unsigned($random)%16;            // between 0 and 15
-    opcode        = opcode_t'($unsigned($random)%8);  // between 0 and 7, cast to opcode_t type
-    write_pointer = temp++;
-  endfunction : randomize_transaction
-
-  function void print_transaction;
+  //function void randomize_transaction();
+   // static int temp = 0;
+    //operand_a     = $random(seed)%16;                 // between -15 and 15
+   // operand_b     = $unsigned($random)%16;            // between 0 and 15
+   // opcode        = opcode_t'($unsigned($random)%8);  // between 0 and 7, cast to opcode_t type
+   // write_pointer = temp++;
+  //endfunction : randomize_transaction
+  constraint operandA_const{
+	operand_a >= -15; 
+	operand_a <=15;
+  }
+  
+  constraint operandB_const{
+	operand_b >= 0; 
+	operand_b <= 15;
+  }
+  
+  constraint opcode_const{
+	opcode >= 0; 
+	opcode <= 7;
+  }
+  
+  virtual function void  print_transaction();
     $display("Writing to register location %0d: ", write_pointer);
     $display("  opcode = %0d (%s)", opcode, opcode.name);
     $display("  operand_a = %0d",   operand_a);
@@ -42,32 +56,76 @@ module instr_register_test (tb_ifc io);  // interface port
 
 // -------------------------------- //
 
+  class Transaction_ext extends Transaction;
+    function void print_transaction();
+	  $display("Clasa extinsa.");
+	  super.print_transaction();
+	endfunction
+  endclass
+
   class Driver;
   virtual tb_ifc vifc;
   Transaction tr;
+  Transaction_ext tr_ext;
 
     function new(virtual tb_ifc vifc);
       this.vifc = vifc;
       tr = new();
+	  tr_ext = new();
     endfunction 
-
-    task generate_transaction();
-      $display("\nReseting the instruction register...");
-      vifc.cb.write_pointer   <= 5'h00;      // initialize write pointer
+	
+	task resetSignals();
+	  vifc.cb.write_pointer   <= 5'h00;      // initialize write pointer
       vifc.cb.read_pointer    <= 5'h1F;      // initialize read pointer
       vifc.cb.load_en         <= 1'b0;       // initialize load control line
       vifc.cb.reset_n         <= 1'b0;       // assert reset_n (active low)
-      repeat (2) @(vifc.cb) ;                // hold in reset for 2 clock cycles
+	  repeat (2) @(vifc.cb) ;                // hold in reset for 2 clock cycles
       vifc.cb.reset_n         <= 1'b1;       // deassert reset_n (active low)
+	endtask
+	
+	function assignSignals();
+		static int temp = 0;
+		vifc.cb.operand_a <= tr.operand_a;
+        vifc.cb.operand_b <= tr.operand_b;
+        vifc.cb.opcode <= tr.opcode;
+        vifc.cb.write_pointer <= temp++;
+	endfunction
+	
+    task generate_transaction(); //consuma timp de simulare 
+	
+      $display("\nReseting the instruction register...");
+	  resetSignals();
+      //vifc.cb.write_pointer   <= 5'h00;      // initialize write pointer
+      //vifc.cb.read_pointer    <= 5'h1F;      // initialize read pointer
+      //vifc.cb.load_en         <= 1'b0;       // initialize load control line
+      //vifc.cb.reset_n         <= 1'b0;       // assert reset_n (active low)
+      //repeat (2) @(vifc.cb) ;                // hold in reset for 2 clock cycles
+      //vifc.cb.reset_n         <= 1'b1;       // deassert reset_n (active low)
 
       $display("\nWriting values to register stack...");
       @vifc.cb vifc.cb.load_en <= 1'b1;      // enable writing to register
       repeat (3) begin
-        @(vifc.cb) tr.randomize_transaction();
-        vifc.cb.operand_a <= tr.operand_a;
-        vifc.cb.operand_b <= tr.operand_b;
-        vifc.cb.opcode <= tr.opcode;
-        vifc.cb.write_pointer <= tr.write_pointer;
+        @(vifc.cb) tr.randomize();
+		assignSignals();
+        //vifc.cb.operand_a <= tr.operand_a;
+        //vifc.cb.operand_b <= tr.operand_b;
+        //vifc.cb.opcode <= tr.opcode;
+        //vifc.cb.write_pointer <= tr.write_pointer;
+        @(vifc.cb) tr.print_transaction();
+      end
+	  //@vifc.cb vifc.cb.load_en <= 1'b0;      // turn-off writing to register
+	  
+	  tr = tr_ext;
+	  
+	  //@vifc.cb vifc.cb.load_en <= 1'b1;      // enable writing to register
+	  
+	  repeat (3) begin
+        @(vifc.cb) tr.randomize();
+		assignSignals();
+        //vifc.cb.operand_a <= tr.operand_a;
+        //vifc.cb.operand_b <= tr.operand_b;
+        //vifc.cb.opcode <= tr.opcode;
+        //vifc.cb.write_pointer <= tr.write_pointer;
         @(vifc.cb) tr.print_transaction();
       end
       @vifc.cb vifc.cb.load_en <= 1'b0;      // turn-off writing to register
@@ -93,7 +151,7 @@ module instr_register_test (tb_ifc io);  // interface port
 
     task transaction_monitor();
       $display("\nReading back the same register locations written...");
-      for (int i=0; i<=2; i++) begin
+      for (int i=0; i<=12; i++) begin
         @(this.vifc.cb) this.vifc.cb.read_pointer <= i;
         @(this.vifc.cb) this.print_results();
       end
